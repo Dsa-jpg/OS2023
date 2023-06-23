1e6168c817eb55cf8c3c4d867df7d3c67953648a
Filip <65063262+Dsa-jpg@users.noreply.github.com>
2023-06-23 13:50:02 +0200

    Update numberOfProgramanRunning.bat

--- numberOfProgramanRunning.bat
+++ numberOfProgramanRunning.bat
@@ -1,3 +1,8 @@
+# Skript zkontroluje, zda je první argument (%~1) prázdný. Pokud je prázdný, použije příkaz tasklist s parametrem find /c "winword.exe", který spočítá počet běžících procesů s názvem "winword.exe".
+
+# Pokud je první argument zadán, použije příkaz tasklist s parametrem find /c "%~1", který spočítá počet běžících procesů s názvem zadaným v argumentu.
+# Nachtman Filip 
+
 @echo off
 IF "%~1" equ "" (
    tasklist | find /c "winword.exe"
